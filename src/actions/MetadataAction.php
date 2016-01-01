57fc080fe8d2906dac173b27dc0a7a2164162faf
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 17:38:35 +0700

    Format code.

--- src/actions/MetadataAction.php
+++ src/actions/MetadataAction.php
@@ -2,20 +2,22 @@
 
 namespace asasmoyo\yii2saml\actions;
 
-use yii\base\Action;
 use Yii;
+use yii\web\Response;
 
 /**
  * This provides action for displaying metadata for this Service Provider. It uses OneLogin_Saml2_Auth configuration and then generate metadata in xml.
  */
-class MetadataAction extends BaseAction {
+class MetadataAction extends BaseAction
+{
 
     /**
      * Display Service Provider Metadata in xml format.
      * @return void
      */
-    public function run() {
-        \Yii::$app->response->format = \yii\web\Response::FORMAT_XML;
+    public function run()
+    {
+        \Yii::$app->response->format = Response::FORMAT_XML;
         echo $this->samlInstance->getMetadata();
     }
 
