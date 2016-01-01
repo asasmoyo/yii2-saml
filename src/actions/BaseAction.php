6ba800ced66d97d529f9c005c4b4d87d9ab41cb0
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 13:13:19 +0700

    Base class for actions.

---
+++ src/actions/BaseAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace asasmoyo\yii2saml\actions;
+
+use yii\base\Action;
+use Yii;
+
+/**
+ * Base class for actions.
+ */
+abstract class BaseAction extends Action {
+
+    /**
+     * This variable should be the component name of asasmoyo\yii2saml\Saml.
+     * @var string
+     */
+    public $samlInstanceName = 'saml';
+
+    /**
+     * This variable hold the instance of asasmoyo\yii2saml\Saml.
+     * @var asasmoyo\yii2saml\Saml
+     */
+    protected $samlInstance;
+
+    public function init() {
+        parent::init();
+
+        $this->samlInstance = Yii::$app->get($this->samlInstanceName);
+    }
+
+}
