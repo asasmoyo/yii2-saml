44d84b573a26e01d00042c721db7db892dc1ae31
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 17:01:11 +0700

    Add bootstrap file for tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(-1);
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
