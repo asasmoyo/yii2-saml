4d481076af77485344cae4722da912715d652614
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 17:39:10 +0700

    Format code.

--- src/actions/BaseAction.php
+++ src/actions/BaseAction.php
@@ -8,7 +8,8 @@ use Yii;
 /**
  * Base class for actions.
  */
-abstract class BaseAction extends Action {
+abstract class BaseAction extends Action
+{
 
     /**
      * This variable should be the component name of asasmoyo\yii2saml\Saml.
@@ -18,11 +19,12 @@ abstract class BaseAction extends Action {
 
     /**
      * This variable hold the instance of asasmoyo\yii2saml\Saml.
-     * @var asasmoyo\yii2saml\Saml
+     * @var \asasmoyo\yii2saml\Saml
      */
     protected $samlInstance;
 
-    public function init() {
+    public function init()
+    {
         parent::init();
 
         $this->samlInstance = Yii::$app->get($this->samlInstanceName);
