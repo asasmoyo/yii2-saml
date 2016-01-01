3e32838658e2a968507c6f787f265a3c99359984
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 17:40:50 +0700

    Delete OneLoginSaml class and format code.

--- tests/SamlTest.php
+++ tests/SamlTest.php
@@ -4,9 +4,11 @@ namespace asasmoyo\yii2saml\tests;
 
 use asasmoyo\yii2saml\Saml;
 
-class SamlTest extends \PHPUnit_Framework_TestCase {
+class SamlTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function testCreateInstance() {
+    public function testCreateInstance()
+    {
         $instance = new Saml();
         $this->assertNotEquals($instance, null);
     }
