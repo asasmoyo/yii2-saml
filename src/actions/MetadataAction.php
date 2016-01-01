e3310d6acd562b464beeaa35b74312cb2d4f89b7
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-01 13:12:44 +0700

    Create base class for actions.

--- src/actions/MetadataAction.php
+++ src/actions/MetadataAction.php
@@ -8,26 +8,12 @@ use Yii;
 /**
  * This provides action for displaying metadata for this Service Provider. It uses OneLogin_Saml2_Auth configuration and then generate metadata in xml.
  */
-class MetadataAction extends Action {
+class MetadataAction extends BaseAction {
 
     /**
-     * This variable should be the component name of asasmoyo\yii2saml\Saml.
-     * @var string
+     * Display Service Provider Metadata in xml format.
+     * @return void
      */
-    public $samlInstanceName = 'saml';
-
-    /**
-     * This variable hold the instance of asasmoyo\yii2saml\Saml.
-     * @var asasmoyo\yii2saml\Saml
-     */
-    private $samlInstance;
-
-    public function init() {
-        parent::init();
-
-        $this->samlInstance = Yii::$app->get($this->samlInstanceName);
-    }
-
     public function run() {
         \Yii::$app->response->format = \yii\web\Response::FORMAT_XML;
         echo $this->samlInstance->getMetadata();
