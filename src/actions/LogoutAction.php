a596f8bac3bd6748cec3ddf6090bdac658b71a82
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2016-01-03 08:48:15 +0700

    Fix logout action.

--- src/actions/LogoutAction.php
+++ src/actions/LogoutAction.php
@@ -19,6 +19,14 @@ class LogoutAction extends BaseAction
      */
     public function run()
     {
+        // Check wether SAMLResponse is present. If it is, we process the response and redirect the user to returnTo.
+        $response = \Yii::$app->request->post('SAMLResponse');
+        if ($response) {
+            $this->samlInstance->processResponse();
+            return \Yii::$app->response->redirect($this->returnTo);
+        }
+
+        // if it isn't, logout the user on Yii 2 and Identity Provider.
         \Yii::$app->user->logout();
         $this->samlInstance->logout($this->returnTo);
     }
