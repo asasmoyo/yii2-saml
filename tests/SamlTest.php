c3399211864d96622c83b2a01877d53bb21826ce
Juliardi <ardi93@gmail.com>
2018-04-28 20:58:45 +0700

    allow set config from array

--- tests/SamlTest.php
+++ tests/SamlTest.php
@@ -6,11 +6,21 @@ use asasmoyo\yii2saml\Saml;
 
 class SamlTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testCreateInstance()
     {
         $instance = new Saml();
         $this->assertNotEquals($instance, null);
     }
 
+    public function testConfigFromArray()
+    {
+        $config = require __DIR__ . '/config/saml.php';
+
+        $instance = new Saml([
+            'config' => $config,
+        ]);
+
+        $this->assertNotEquals($instance->config, []);
+        $this->assertEquals($instance->config['sp']['entityId'], 'service-provider');
+    }
 }
